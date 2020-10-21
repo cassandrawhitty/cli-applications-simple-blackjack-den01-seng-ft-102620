1c5cdc3271b4b5bc36b6d3ff9ddbec74b3dcdb65
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-21 23:34:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,6 +54,9 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  initial_round
+  hit?(card_total)
+  display_card_total(card_total)
 end
     
