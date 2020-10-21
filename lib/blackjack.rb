1e84811101a9f40024717d50473dc4018ea23aef
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-21 23:18:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,7 +30,13 @@ def initial_round
   return card_total
 end
 
-def hit?
+def hit?(card_total)
+  prompt_user
+  get_user_input
+  if get_user_input == 'h'
+    card_3 = deal_card
+    card_total += card_3
+  end
   # code hit? here
 end
 
