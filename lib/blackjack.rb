34c300934fbd6e64d9ac3ece498bb4ba4cc841e4
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-21 23:36:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,7 +58,9 @@ def runner
   user_hand = initial_round
   card_total = user_hand
   until card_total >= 21
-  card_total = hit?(card_total)
-  display_card_total(card_total)
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  
 end
     
